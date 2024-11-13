3bd2a128c9ef92859d1ebf149bfbc681a406cbb1
SunLYous <ravan057@bk.ru>
2024-11-13 13:14:55 +0300

    complete

---
+++ docs/scripts/pageLoad.js
@@ -0,0 +1,25 @@
+﻿(function() {
+    window.addEventListener('load', function() {
+        const footer = document.querySelector('.footer');
+
+        if (footer) {
+            const loadTime = performance.now().toFixed(2);
+            const loadInfo = document.createElement('p');
+            loadInfo.textContent = `Page Load Time: ${loadTime} ms`;
+            footer.appendChild(loadInfo);
+        } else {
+            console.error("Footer element not found.");
+        }
+    });
+})();
+
+
+(function () {
+    const currentPath = document.location.pathname;
+    const menuItems = document.querySelectorAll('.menu .list li a');
+    menuItems.forEach((menuItem) => {
+        if (menuItem.getAttribute('href') === currentPath) {
+            menuItem.classList.add('active');
+        }
+    });
+})();
